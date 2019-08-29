43f4b8f0a612822b920f1ebacb70c8ea6b41328e
Reinaldo Felipe Soares Araujo <felipeiex@hotmail.com>
2019-08-29 10:41:48 -0300

    Pie Graphic

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const server = http.createServer((request, response) => {
 })
 
 server.listen(3000, () => {
-  console.log('Running at http://localhost:3000');
+  console.log('Running at http://localhost:3000/?grupCategory=1');
 });
 
 
